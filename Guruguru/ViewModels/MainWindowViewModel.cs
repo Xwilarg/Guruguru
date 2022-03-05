9475a47b38ba5b3f469f5741a37756bf03c7e720
Xwilarg <xwilarg@yahoo.fr>
2022-03-05 17:38:45 +0100

    Add ability to display images

--- Guruguru/ViewModels/MainWindowViewModel.cs
+++ Guruguru/ViewModels/MainWindowViewModel.cs
@@ -1,5 +1,10 @@
+using Avalonia.Media.Imaging;
 using ReactiveUI;
+using System;
+using System.IO;
+using System.Linq;
 using System.Reactive;
+using System.Threading;
 using System.Windows.Input;
 
 namespace Guruguru.ViewModels
@@ -12,11 +17,46 @@ namespace Guruguru.ViewModels
             {
                 GetFolder.Handle(Unit.Default).Subscribe(Observer.Create<string>(folder =>
                 {
+                    _images = Directory.GetFiles(folder);
                 }));
             });
+
+            new Thread(new ThreadStart(ChangeImage)).Start();
         }
+
+        public void ChangeImage()
+        {
+            while (Thread.CurrentThread.IsAlive)
+            {
+                if (_images.Any())
+                {
+                    if (_index >= _images.Length - 1)
+                    {
+                        _index = 0;
+                    }
+                    else
+                    {
+                        _index++;
+                    }
+
+                    using var stream = File.OpenRead(_images[_index]);
+                    ImageSource = Bitmap.DecodeToWidth(stream, 500);
+                }
+                Thread.Sleep(2000);
+            }
+        }
+
+        private string[] _images = Array.Empty<string>();
+        private int _index;
 
         public Interaction<Unit, string> GetFolder { get; } = new();
         public ICommand SelectFolder { get; }
+
+        private Bitmap _imageSource;
+        public Bitmap ImageSource
+        {
+            get => _imageSource;
+            set => this.RaiseAndSetIfChanged(ref _imageSource, value);
+        }
     }
 }
