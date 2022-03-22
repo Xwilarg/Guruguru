6e7a69a8f055e217a97b9b1b768e25d1551065ea
Christian Chaux <christian.chaux@bionomeex.com>
2022-03-22 18:33:45 +0100

    Add crash handling

--- Guruguru/Program.cs
+++ Guruguru/Program.cs
@@ -1,7 +1,8 @@
 using Avalonia;
-using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.ReactiveUI;
 using System;
+using System.IO;
+using System.Runtime.InteropServices;
 
 namespace Guruguru
 {
@@ -11,8 +12,25 @@ namespace Guruguru
         // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
         // yet and stuff might break.
         [STAThread]
-        public static void Main(string[] args) => BuildAvaloniaApp()
-            .StartWithClassicDesktopLifetime(args);
+        public static void Main(string[] args)
+        {
+            try
+            {
+                BuildAvaloniaApp()
+                .StartWithClassicDesktopLifetime(args);
+            }
+            catch (Exception ex)
+            {
+                File.WriteAllText($"logs-{DateTime.Now:yyyy-MM-dd-HH-mm-ss}.log", ex.ToString());
+                if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                {
+                    _ = MessageBox(IntPtr.Zero, $"The application crashed, more information were saved at {Directory.GetCurrentDirectory()}", ex.GetType().ToString(), 0x00000000L);
+                }
+            }
+        }
+
+        [DllImport("User32.dll")]
+        public static extern int MessageBox(IntPtr handle, string text, string caption, long type);
 
         // Avalonia configuration, don't remove; also used by visual designer.
         public static AppBuilder BuildAvaloniaApp()
